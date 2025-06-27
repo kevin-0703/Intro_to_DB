2132f079d905fb8d9a5b24d120fc699c0ac126e1
KEVIN NSHUTI <nkevin881@gmail.com>
2025-06-27 12:01:48 +0200

    adding data into our database

--- task_5.sql
+++ task_5.sql
@@ -1,11 +1,2 @@
-CREATE DATABASE IF NOT EXISTS alx_book_store;
-USE alx_book_store;
-CREATE TABLE Customer (
-    customer_id INT PRIMARY KEY,
-    customer_name VARCHAR(215) NOT NULL,
-    email VARCHAR(215),
-    address TEXT
-);
-
 INSERT INTO Customers (customer_id, customer_name, email, address)
 VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
